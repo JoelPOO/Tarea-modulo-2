cd41dfcbc057ba03841a0fed053f8a725c280444
JoelPOO <jo.herrera2590@gmail.com>
2024-07-18 15:57:59 -0600

    Add files via upload

---
+++ Proyecto1.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.proyecto1;
+
+
+public class Proyecto1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hola soy Milton Joel Sarmiento");
+    }
+}
